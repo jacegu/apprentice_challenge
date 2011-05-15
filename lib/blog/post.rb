d4ee97f84dfe11b1568685f60fc9205dd659b9c1
Javier Acero <j4cegu@gmail.com>
2011-05-15 13:30:36 +0200

    post has a publication time

--- lib/blog/post.rb
+++ lib/blog/post.rb
@@ -1,10 +1,13 @@
+require 'date'
+
 module Blog
   class Post
-    attr_reader :title, :description
+    attr_reader :title, :description, :publication_time
 
-    def initialize(title, description)
+    def initialize(title, description, publication_time)
       @title = title
       @description = description
+      @publication_time = publication_time
     end
   end
 end
--- test/blog/post_tests.rb
+++ test/blog/post_tests.rb
@@ -3,11 +3,15 @@ $: << File.join(File.expand_path(File.dirname(__FILE__)), '..')
 require 'test_helper'
 
 def a_post_entitled(the_title)
-  Blog::Post.new(the_title, '')
+  Blog::Post.new(the_title, '', DateTime.new)
 end
 
 def a_post_with_description(the_description)
-  Blog::Post.new('', the_description)
+  Blog::Post.new('', the_description, DateTime.new)
+end
+
+def a_post_with_publication_time(the_time)
+  Blog::Post.new('', '', the_time)
 end
 
 module Blog
@@ -21,5 +25,11 @@ module Blog
     the_description = 'the post description'
     post = a_post_with_description(the_description)
     assert_equal post.description, the_description
+  end
+
+  test 'a post has a time it will be published at' do
+    the_time = DateTime.new
+    post = a_post_with_publication_time(the_time)
+    assert_equal post.publication_time, the_time
   end
 end
