105e62cf23542d50c25734cd85e00e2cbbe99256
Javier Acero <j4cegu@gmail.com>
2011-05-15 17:31:39 +0200

    renamed tests

--- test/blog/post_tests.rb
+++ test/blog/post_tests.rb
@@ -34,23 +34,23 @@ module Blog
     assert_true other_post > a_post
   end
 
-  test 'a post is published if its publication time is present or past' do
+  test 'post is published if its publication time is present or past' do
     published_post = a_post_with_publication_time(DateTime.parse('2011-01-01 00:00:00'))
     assert_true published_post.published?
   end
 
-  test 'a post has an uri which is generated from the title' do
+  test '#uri - a post has its title as uri' do
     post_title = 'title'
     post = a_post_entitled(post_title)
     assert_equal post.uri, post_title
   end
 
-  test 'spaces are replaces with - in the title uri' do
+  test '#uri - spaces are replaced with scripts ("-") in the uri' do
     post = a_post_entitled('title with several words')
     assert_equal post.uri, 'title-with-several-words'
   end
 
-  test 'non alphanumeric characters are removed from the url' do
+  test '#uri - non alphanumeric characters are removed from the uri' do
     post = a_post_entitled('t1?t)l%3 w1#t@h s0ºm3 [s+ym=b0ls]')
     assert_equal post.uri, 't1tl3-w1th-s0m3-symb0ls'
   end
