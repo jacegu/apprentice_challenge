286c9c173857325dc3d5e4eb4f8b73439d2317c0
Javier Acero <j4cegu@gmail.com>
2011-05-15 14:13:22 +0200

    post creation helper methdos available for every test

--- test/blog/post_tests.rb
+++ test/blog/post_tests.rb
@@ -2,22 +2,6 @@ $: << File.join(File.expand_path(File.dirname(__FILE__)), '..')
 
 require 'test_helper'
 
-def a_post_entitled(the_title)
-  Blog::Post.new(the_title, '', DateTime.new, '')
-end
-
-def a_post_with_description(the_description)
-  Blog::Post.new('', the_description, DateTime.new, '')
-end
-
-def a_post_with_publication_time(the_time)
-  Blog::Post.new('', '', the_time, '')
-end
-
-def a_post_with_content(the_content)
-  Blog::Post.new('', '', DateTime.new, the_content)
-end
-
 module Blog
   test 'a post has a title' do
     the_title = 'the post title'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,19 @@ $: << File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib')
 
 require 'testing'
 require 'blog'
+
+def a_post_entitled(the_title)
+  Blog::Post.new(the_title, '', DateTime.new, '')
+end
+
+def a_post_with_description(the_description)
+  Blog::Post.new('', the_description, DateTime.new, '')
+end
+
+def a_post_with_publication_time(the_time)
+  Blog::Post.new('', '', the_time, '')
+end
+
+def a_post_with_content(the_content)
+  Blog::Post.new('', '', DateTime.new, the_content)
+end
