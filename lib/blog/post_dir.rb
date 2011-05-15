ae542851a609edf3cf729c815ad74b7610299928
Javier Acero <j4cegu@gmail.com>
2011-05-15 23:21:41 +0200

    post from post dir

--- lib/blog/post_dir.rb
+++ lib/blog/post_dir.rb
@@ -1,9 +1,19 @@
-class PostDir
-  attr_reader :entries
+module Blog
+  class PostDir
+    attr_reader :entries, :path
 
-  POST_FILE_NAME_PATTERN = /.*\.post\.html/
+    POST_FILE_NAME_PATTERN = /.*\.post\.html/
 
-  def initialize(dir)
-    @entries = dir.entries.select{ |e| e =~ POST_FILE_NAME_PATTERN  }
+    def initialize(dir)
+      @path = dir.path
+      @entries = dir.entries.select{ |e| e =~ POST_FILE_NAME_PATTERN  }
+    end
+
+    def posts
+      @entries.map do |entry|
+        entry_path = File.join(@path, entry)
+        File.open(entry_path, 'r'){ |f| PostFile.new(f) }
+      end
+    end
   end
 end
--- test/blog/post_dir_tests.rb
+++ test/blog/post_dir_tests.rb
@@ -1,11 +1,13 @@
 $: << File.join(File.expand_path(File.dirname(__FILE__)), '..')
 
 require 'test_helper'
+require 'file_double'
 
 class DirDouble
   attr_reader :path, :entries
 
-  def initialize(entries)
+  def initialize(entries, path)
+    @path = path
     @entries = entries
     @entries << '.'
     @entries << '..'
@@ -17,10 +19,25 @@ module Blog
              '.hidden_file',
              'post-file.post.html']
 
-  the_directory = DirDouble.new(entries)
+  the_directory = DirDouble.new(entries, '/')
   @dir = PostDir.new(the_directory)
+
+  test 'directory has a path' do
+    assert_equal @dir.path , '/'
+  end
 
   test 'directory entries are only post files' do
     assert_equal @dir.entries, ['post-file.post.html']
+  end
+
+  test '#posts - return a post file for each post in the directory' do
+    publication_time = DateTime.parse('2011-01-01 10:00')
+    expected_post = Post.new('title', 'desc', 'content', publication_time)
+
+    file = FileDouble.new("2011-01-01 10:00\ntitle\ndesc\ncontent")
+    #File.yield_file_when_opening(file, '/post-file.post.html')
+
+    assert_equal PostFile.new(file), expected_post
+    #assert_true @dir.posts.include?(expected_post)
   end
 end
