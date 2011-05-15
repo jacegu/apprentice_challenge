884eeebb3ad75cebed111d682dfc9fc822453e82
Javier Acero <j4cegu@gmail.com>
2011-05-15 18:28:21 +0200

    title is extracted from the second line with text of the file

--- lib/blog/post_file.rb
+++ lib/blog/post_file.rb
@@ -1,9 +1,22 @@
 module Blog
   class PostFile
+
+    TITLE_LINE = 1
+
     attr_reader :full_content
 
     def initialize(file)
       @full_content = file.read
+    end
+
+    def title
+      lines[TITLE_LINE]
+    end
+
+    def lines
+      lines = @full_content.split("\n")
+      lines.each{ |l| l.lstrip! }
+      lines.reject!{ |l| l.length == 0 }
     end
   end
 end
--- test/blog/post_file_tests.rb
+++ test/blog/post_file_tests.rb
@@ -2,25 +2,49 @@ $: << File.join(File.expand_path(File.dirname(__FILE__)), '..')
 
 require 'test_helper'
 
+class FileDouble
+  def initialize(content)
+    @content = content
+  end
+
+  def read
+    @content
+  end
+end
+
+def sample_post_file_content
+  """
+  2011-05-08 20:00
+ 
+  
+  This is the post title
+   
+    
+  This should be the post description
+     
+  <h2>The content</h2>
+      
+  This is part of the content of the post
+  <p>And this is also part of the content of the post</p>
+  """
+end
 
 
 module Blog
-
-  class FileDouble
-    def initialize(content)
-      @content = content
-    end
-
-    def read
-      @content
-    end
-  end
+  the_file = FileDouble.new(sample_post_file_content)
+  @post_file = PostFile.new(the_file)
 
   test 'a post file reads the full content of the File its created from' do
-    the_content = 'the content of the file'
-    the_file = FileDouble.new(the_content)
-    post_file = PostFile.new(the_file)
-    assert_equal post_file.full_content, the_content
+    assert_equal @post_file.full_content, sample_post_file_content
   end
 
+  xtest '#publication_time takes the first line with text as the publication time'
+
+  test '#title takes the second line with text as the title' do
+    assert_equal @post_file.title, 'This is the post title'
+  end
+
+  xtest '#description takes the third line with text as the description'
+
+  xtest '#content takes from 4th line to the end of the file as the content'
 end
