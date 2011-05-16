0be5bfb8db28057838bb07f6e492bca5f8e2072e
Javier Acero <j4cegu@gmail.com>
2011-05-16 12:27:51 +0200

    main page does not show not published posts

--- test/my_blog/engine_tests.rb
+++ test/my_blog/engine_tests.rb
@@ -83,7 +83,10 @@ module MyBlog
     assert_contains response.body, 'The 2nd post'
   end
 
-  xtest 'main page does not display the posts that have not been published yet'
+  test 'main page does not display the posts that have not been published yet' do
+    response = get '/blog'
+    assert_does_not_contain response.body, 'Not published'
+  end
 
   xtest 'post page displays only the requested post'
 
