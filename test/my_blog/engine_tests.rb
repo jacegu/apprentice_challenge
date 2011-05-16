003934a5e7a2e534dfaab00766c08ac4620bd3e8
Javier Acero <j4cegu@gmail.com>
2011-05-16 11:36:24 +0200

    temporal posts for engine testing

--- test/my_blog/engine_tests.rb
+++ test/my_blog/engine_tests.rb
@@ -1,6 +1,7 @@
 $: << File.join(File.expand_path(File.dirname(__FILE__)), '..')
 
 require 'test_helper'
+require 'tmpdir'
 
 module MyBlog
   test 'Engine::Request is created from a request path uri' do
@@ -32,5 +33,28 @@ module MyBlog
   test 'Engine::Request returns empty string as post uri on main page request' do
     request = Engine::Request.new("/blog")
     assert_equal request.post_uri, ''
+  end
+
+  create_temporal_posts
+
+  xtest 'main page displays all the published posts'
+
+  xtest 'post page displays only the requested post'
+
+  xtest 'if the requested post doesnt exists returns a 404'
+
+  xtest 'if the requested post is not published returns a 404'
+end
+
+def create_temporal_posts
+  post_dir_path = Dir.tmpdir
+  post1 = "2011-05-08 10:00\nThe 1st post\nThe 1st post desc\n1st post content"
+  post2 = "2022-05-02 09:00\nThe 2st post\nThe 2st post desc\n2st post content"
+  post3 = "9999-12-31 23:59\nNot published\nNot published\nNot published"
+  i = 1
+  [post1, post2, post3].each do |post_content|
+    post_file_path = File.join(post_dir_path, "post-#{i}.post.html")
+    File.open(post_file_path, 'w+'){ |f| f.puts post_content}
+    i += 1
   end
 end
