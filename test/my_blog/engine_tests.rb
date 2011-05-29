55d05802bbb422e0acfea7ced8e3a88a3c5d5b93
Javier Acero <j4cegu@gmail.com>
2011-05-29 21:06:23 +0200

    feed double to its own file

--- test/my_blog/engine_tests.rb
+++ test/my_blog/engine_tests.rb
@@ -1,45 +1,9 @@
 $: << File.join(File.expand_path(File.dirname(__FILE__)), '..')
 
 require 'test_helper'
-require 'uri'
-require 'net/http'
+require 'feed_double'
 
 PORT = 8583
-
-class FeedDouble
-  def read
-  %{
-<?xml version='1.0' encoding='utf-8' ?>
-<rss version='2.0'
-     xmlns:atom='http://www.w3.org/2005/Atom'
-     xmlns:content='http://purl.org/rss/1.0/modules/content/'>
-  <channel>
-    <title>Testing RSS Feed</title>
-    <link>http:/localhost:8583/blog/rss</link>
-    <description>Testing RSS Feed</description>
-    <item>
-      <title>The 1st post</title>
-      <description>The 1st post desc</description>
-      <pubDate>Tue, 08 May 2011 10:00:00 +0000</pubDate>
-      <content:encoded>1st post content</content:encoded>
-    </item>
-    <item>
-      <title>The 2st post</title>
-      <description>The 2st post desc</description>
-      <pubDate>Tue, 02 May 2011 09:00:00 +0000</pubDate>
-      <content:encoded>2st post content</content:encoded>
-    </item>
-    <item>
-      <title>Not published</title>
-      <description>Not published</description>
-      <pubDate>Fri, 31 dec 9999 23:59:59 +0000</pubDate>
-      <content:encoded>Not published</content:encoded>
-    </item>
-  </channel>
-</rss>
-  }
-  end
-end
 
 def run_engine
   feed = FeedDouble.new
