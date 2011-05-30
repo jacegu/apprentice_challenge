02761f27ee029790d38fe5a0d534005b934730ca
Javier Acero <j4cegu@gmail.com>
2011-05-30 12:06:30 +0200

    post page displays only requested post

--- test/my_blog/engine_tests.rb
+++ test/my_blog/engine_tests.rb
@@ -105,7 +105,7 @@ module MyBlog
     assert_does_not_contain response.body, 'Not published'
   end
 
-  xtest 'post page displays only the requested post' do
+  test 'post page displays only the requested post' do
     response = get '/blog/the-1st-post'
     assert_contains response.body, 'The 1st post'
     assert_does_not_contain response.body, 'The 2nd post'
