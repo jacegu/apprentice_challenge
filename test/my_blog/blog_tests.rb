504e57283368bc3b65131de5f8cfbe244cecfb4f
Javier Acero <j4cegu@gmail.com>
2011-05-30 11:58:37 +0200

    file double removed and dir double replaced with feed double

--- test/my_blog/blog_tests.rb
+++ test/my_blog/blog_tests.rb
@@ -2,7 +2,7 @@ $: << File.join(File.expand_path(File.dirname(__FILE__)), '..')
 
 require 'test_helper'
 
-class PostDirDouble
+class PostFeedDouble
   attr_reader :posts
 
   def initialize(posts)
@@ -10,25 +10,27 @@ class PostDirDouble
   end
 end
 
+EMPTY_POST_FEED = PostFeedDouble.new([])
+
 module MyBlog
   test 'a blog has a name' do
     the_name = 'ecomba challenge blog'
-    blog = Blog.new(the_name, 'desc', [])
+    blog = Blog.new(the_name, 'desc', EMPTY_POST_FEED)
 
     assert_equal blog.name, the_name
   end
 
   test 'a blog has a description' do
     the_description = 'the blog has a description'
-    blog = Blog.new('name', the_description, [])
+    blog = Blog.new('name', the_description, EMPTY_POST_FEED)
 
     assert_equal blog.description, the_description
   end
 
-  test 'a blog has post dir where it takes its posts from' do
+  test 'a blog has a post feed where it takes its posts from' do
     the_posts = [a_post_entitled('sample post'), a_post_entitled('another post')]
-    post_dir = PostDirDouble.new(the_posts)
-    blog = Blog.new('name', 'desc', post_dir)
+    post_feed = PostFeedDouble.new(the_posts)
+    blog = Blog.new('name', 'desc', post_feed)
 
     assert_equal blog.posts, the_posts
   end
@@ -38,8 +40,8 @@ module MyBlog
     second_post = a_post_with_publication_time(DateTime.parse('2011-01-01 09:00:01'))
     third_post  = a_post_with_publication_time(DateTime.parse('2011-01-01 09:00:02'))
 
-    post_dir = PostDirDouble.new([third_post, first_post, second_post])
-    blog = Blog.new('name', 'desc', post_dir)
+    post_feed = PostFeedDouble.new([third_post, first_post, second_post])
+    blog = Blog.new('name', 'desc', post_feed)
 
     assert_equal blog.posts[2], first_post
     assert_equal blog.posts[1], second_post
@@ -50,8 +52,8 @@ module MyBlog
     published     = a_post_with_publication_time(DateTime.parse('2011-01-01 09:00:00'))
     not_published = a_post_with_publication_time(DateTime.parse('9999-01-01 09:00:01'))
 
-    post_dir = PostDirDouble.new([published, not_published])
-    blog = Blog.new('name', 'desc', post_dir)
+    post_feed = PostFeedDouble.new([published, not_published])
+    blog = Blog.new('name', 'desc', post_feed)
 
     assert_equal blog.published_posts, [published]
   end
@@ -60,15 +62,15 @@ module MyBlog
     searched      = Post.new('post title', 'd', 'c', DateTime.parse('2011-01-01 09:00:00'))
     not_published = a_post_with_publication_time(DateTime.parse('9999-01-01 09:00:01'))
 
-    post_dir = PostDirDouble.new([searched, not_published])
-    blog = Blog.new('name', 'desc', post_dir)
+    post_feed = PostFeedDouble.new([searched, not_published])
+    blog = Blog.new('name', 'desc', post_feed)
 
     assert_equal blog.post_with_uri('post-title'), searched
   end
 
   test '#post_with_uri - returns null post if no post with given uri exists' do
     null_post = NullPost.new
-    blog = Blog.new('name', 'desc', PostDirDouble.new([]))
+    blog = Blog.new('name', 'desc', EMPTY_POST_FEED)
     assert_equal blog.post_with_uri('any-uri'), null_post
   end
 end
