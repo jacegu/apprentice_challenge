34795875cb23c27d68726f85ae1fc2bac0e117f4
Javier Acero <j4cegu@gmail.com>
2011-06-02 13:37:13 +0200

    more feeds to test

--- bin/run_engine.rb
+++ bin/run_engine.rb
@@ -9,10 +9,23 @@ default_port     = 8583
 blog_name        = '@ecomba apprentice challenge blog'
 blog_description = 'This blog engine uses only The Ruby Language core and has been created as an answer to the challenge that Enrique Comba threw to anyone who wanted to be his apprentice.'
 
-#feed_uri = 'http://ecomba.org/blog/rss'
-#feed_uri = 'http://javieracero.com/blog/rss'
+# RSS
 #feed_uri = 'http://feeds.feedburner.com/SeHaceCaminoAlAndar?format.xml'
-feed_uri = 'http://www.carlosble.com/feed/'
+#feed_uri = 'http://www.carlosble.com/feed/'
+#feed_uri = 'http://feeds2.feedburner.com/CoreysPairProgrammingTour'
+#feed_uri = 'http://blog.rubybestpractices.com/feed/gregory.xml'
+#feed_uri = 'http://sermoa.wordpress.com/feed'
+#feed_uri = 'http://feeds.feedburner.com/obtiva'
+feed_uri = 'http://www.tuaw.com/rss.xml'
+
+# Atom
+#feed_uri = 'http://feeds.feedburner.com/YoYElSoftware?format=xml'
+#feed_uri = 'http://pragdave.blogs.pragprog.com/pragdave/atom.xml'
+#feed_uri = 'http://martinfowler.com/feed.atom'
+#feed_uri = 'http://feeds.feedburner.com/RidingRails'
+#feed_uri = 'http://thecleancoder.blogspot.com/feeds/posts/default'
+#feed_uri = 'http://blog.8thlight.com/feed/atom.xml'
+#feed_uri = 'http://chadfowler.com/posts.atom'
 
 feed = MyBlog::PostFeed.new(open(feed_uri))
 blog = MyBlog::Blog.new(blog_name, blog_description, feed)
